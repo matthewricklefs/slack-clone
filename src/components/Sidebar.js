2c49d7908e841335bd6ad6992a1dc6cbf22479e7
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-03-25 15:13:29 -0400

    adds numerous materialui icons to pass as props into the SidebarOption component inside of the SidebarContainer

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,16 @@ import React from "react";
 import styled from "styled-components";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import CreateIcon from "@material-ui/icons/Create";
+import InsertCommentIcon from "@material-ui/icons/InsertComment"
+import InboxIcon from "@material-ui/icons/Inbox"
+import DraftsIcon from "@material-ui/icons/Drafts"
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder"
+import PeopleAltIcon from "@material-ui/icons/FileCopy"
+import AppsIcon from "@material-ui/icons/PeopleAlt"
+import FileCopyIcon from "@material-ui/icons/Apps"
+import ExpandLessIcon from "@material-ui/icons/ExpandLess"
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
+import SidebarOption from "./SidebarOption";
 
 function Sidebar() {
   return (
@@ -17,7 +27,15 @@ function Sidebar() {
         <CreateIcon />
       </SidebarHeader>
 
-      <SidebarOption></SidebarOption>
+      <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+      <SidebarOption Icon={InboxIcon} title="Mentions & Reactions" />
+      <SidebarOption Icon={DraftsIcon} title="Saved Items" />
+      <SidebarOption Icon={BookmarkBorderIcon} title="Channel Browser" />
+      <SidebarOption Icon={PeopleAltIcon} title="People & User Groups" />
+      <SidebarOption Icon={AppsIcon} title="Apps" />
+      <SidebarOption Icon={FileCopyIcon} title="File Browser" />
+      <SidebarOption Icon={ExpandLessIcon} title="Show Less" />
+      <SidebarOption Icon={ExpandMoreIcon} title="Show More" />
     </SidebarContainer>
   );
 }
--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 function SidebarOption() {
   return (
     <div>
-      
+      <SideBarOptionContainer></SideBarOptionContainer>
     </div>
-  )
+  );
 }
 
-export default SidebarOption
+export default SidebarOption;
+
+const SideBarOptionContainer = styled.div``;

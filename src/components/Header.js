f6d512fce8f10528b9214dd9dca9a4a3b642756a
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-03-25 14:22:12 -0400

    install material ui icons and styled-components, begins building out the Header.js component

---
+++ src/components/Header.js
@@ -0,0 +1,48 @@
+import React from "react";
+import styled from "styled-components";
+import { Avatar } from "@material-ui/core";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+
+function Header() {
+  return (
+    <HeaderContainer>
+      {/* Header left */}
+      <HeaderLeft>
+        <HeaderAvatar
+        // TODO: Add onclick
+        />
+        <AccessTimeIcon />
+      </HeaderLeft>
+
+      {/* Header Search */}
+
+      {/* Header right */}
+    </HeaderContainer>
+  );
+}
+
+export default Header;
+
+const HeaderContainer = styled.div`
+  display: flex;
+`;
+
+const HeaderLeft = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+  }
+`;
+
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
